16e60dd1b9e366dda9cbd593468a0a9cfbcd5333
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-24 12:53:41 -0700

    test order constructor for fail

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -8,6 +8,11 @@ namespace VendorTracker.Tests
   [TestClass]
   public class OrderTest
   {
-    
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
