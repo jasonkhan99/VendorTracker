4f26c51417f4c79110f365dc7aadb26bf568fa94
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-24 14:02:10 -0700

    test adding orders to vendor and pass

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -8,6 +8,7 @@ namespace VendorTracker.Models
     public string Name { get; set; }
     public string Address { get; set; }
     public int Id { get; }
+    public List<Order> Orders { get; set; }
 
     public Vendor(string vendorName, string address)
     {
@@ -15,6 +16,7 @@ namespace VendorTracker.Models
       Name = vendorName;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order> { };
     }
 
     public static void ClearAll()
@@ -30,6 +32,11 @@ namespace VendorTracker.Models
     public static Vendor Find(int searchId)
     {
       return _instances[searchId - 1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
     }
   }
 }
