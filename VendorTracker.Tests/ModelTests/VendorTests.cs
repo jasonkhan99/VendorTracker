ba080ecd83ebf162509324d71770b8460da63035
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-24 17:04:49 -0700

    add item dictionary to order

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -82,7 +82,8 @@ namespace VendorTracker.Tests
     {
       string date = "7/24/2020";
       string address = "Broadway";
-      Order newOrder = new Order(date);
+      object items = (1, "pastry");
+      Order newOrder = new Order(date, items);
       List<Order> newList = new List<Order> { newOrder };
       string name = "Test Vendor";
       Vendor newVendor = new Vendor(name, address);
